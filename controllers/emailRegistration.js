77bd78b5fc7ff576259dab5dac83863d469a5f30
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-25 21:51:10 +0200

    fixing the error

--- controllers/emailRegistration.js
+++ controllers/emailRegistration.js
@@ -1,6 +1,7 @@
 import Events from '../models/events.model.js';
 
 export default async function addEmail(req, res) {
+  console.log(req.body);
   const { email, eventId } = req.body;
 
   const event = await Events.findById(eventId);
@@ -8,14 +9,13 @@ export default async function addEmail(req, res) {
     return res.status(400).json({ error: 'Event does not exist' });
   }
 
-  const emailExists = await event.emails_registered.find(
-    (e) => e.email === email
-  );
+  const emailExists = await event.emails_registered.find((e) => e === email);
+
   if (emailExists) {
     return res.status(400).json({ error: 'Email already exists' });
-  } else {
-    event.emails_registered.push({ email });
-    await event.save();
-    return res.status(200).json({ message: 'Email added' });
   }
+
+  event.emails_registered.push(email);
+  await event.save();
+  return res.status(200).json({ message: 'Email added' });
 }
