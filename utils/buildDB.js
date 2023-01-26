a236421b5ebd47ecdbfd4cd5ccf1e61152f3f782
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-26 20:22:51 +0200

    adding drop to the collections in the database

--- utils/buildDB.js
+++ utils/buildDB.js
@@ -7,6 +7,11 @@ import data from '../data/data.json' assert { type: 'json' };
 
 config();
 
+function handleError(err) {
+  console.error(err);
+  process.exit(1);
+}
+
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const cluster = process.env.CLUSTER;
@@ -17,37 +22,39 @@ mongoose.connect(
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function () {
+db.once('open', async () => {
   // console.log(process.argv);
 
-  // Categories.collection.drop();
-  // Events.collection.drop();
-  // function (err) {
-  //   console.log('Categories collection dropped');
-  //   if (err) return handleError(err);
-  // }
-
-  Categories.insertMany(data.events_categories, (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Categories data saved successfully!');
-    }
-  });
-
-  Events.insertMany(
-    data.allEvents.map((e) => {
-      const index = Math.floor(Math.random() * 3);
-      e.category = data.events_categories[index];
-      return e;
-    }),
-    (error, data) => {
+  try {
+    await Categories.collection.drop();
+    console.log('Categories collection dropped');
+    await Events.collection.drop();
+    console.log('Events collection dropped');
+    await Categories.insertMany(data.events_categories, (error, data) => {
       if (error) {
         console.log(error);
       } else {
-        console.log('Events data saved successfully!');
+        console.log('Categories data saved successfully!');
       }
-    }
-  );
+    });
+
+    await Events.insertMany(
+      data.allEvents.map((e) => {
+        const index = Math.floor(Math.random() * 3);
+        e.category = data.events_categories[index];
+        return e;
+      }),
+      (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Events data saved successfully!');
+        }
+      }
+    );
+  } catch (err) {
+    return handleError(err);
+  }
+
   console.log('Connected successfully');
 });
