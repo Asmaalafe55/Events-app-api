3dc9287a6633f5e2b6b22878ba32e1325db487e1
Asmaalafe55 <asmaalafe55@gmail.com>
2023-06-22 21:21:06 +0300

    input validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,27 +5,41 @@ import jwt from 'jsonwebtoken';
 import catchAsync from '../utils/catchAsync.js';
 import httpStatus from 'http-status';
 import { config } from 'dotenv';
+import Joi from 'joi';
 config();
 
 const SECRET = process.env.JWT_SECRET;
 
-export const login = catchAsync(async (req, res) => {
-  const { email, password } = req.body;
+const registerValidationSchema = Joi.object({
+  fName: Joi.string().required(),
+  lName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
 
-  if (!email || !password) {
-    throw new ApiError(
-      httpStatus.BAD_REQUEST,
-      'Please provide email and password'
-    );
+const loginValidationSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const login = catchAsync(async (req, res) => {
+  const { error, value } = loginValidationSchema.validate(req.body);
+  if (error) {
+    throw new ApiError(httpStatus.BAD_REQUEST, error.details[0].message);
   }
+
+  const { email, password } = value;
+
   const user = await Users.findOne({ email });
   if (!user) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'User not found');
   }
+
   const isMatch = bcrypt.compareSync(password, user.password);
   if (!isMatch) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid credentials');
   }
+
   const token = jwt.sign({ id: user._id }, SECRET);
 
   res.status(httpStatus.OK).send({
@@ -36,15 +50,18 @@ export const login = catchAsync(async (req, res) => {
 });
 
 export const register = catchAsync(async (req, res) => {
-  const { fName, lName, email, password } = req.body;
-
-  if (!fName || !lName || !email || !password) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Please provide all fields');
+  const { error, value } = registerValidationSchema.validate(req.body);
+  if (error) {
+    throw new ApiError(httpStatus.BAD_REQUEST, error.details[0].message);
   }
+
+  const { fName, lName, email, password } = value;
+
   const userExists = await Users.findOne({ email });
-  if (!!userExists) {
+  if (userExists) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'User already exists');
   }
+
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
 
@@ -62,12 +79,10 @@ export const register = catchAsync(async (req, res) => {
     );
   }
 
-  res
-    .status(httpStatus.CREATED)
-    .send({
-      id: newUser._id,
-      email: newUser.email,
-      fName: newUser.fName,
-      lName: newUser.lName,
-    });
+  res.status(httpStatus.CREATED).send({
+    id: newUser._id,
+    email: newUser.email,
+    fName: newUser.fName,
+    lName: newUser.lName,
+  });
 });
