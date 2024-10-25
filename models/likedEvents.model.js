ad8e75bfecd31c802afd45c6ed9ca7a3ee16d2e5
Asmaalafe55 <asmaalafe55@gmail.com>
2024-10-25 17:11:20 +0300

    commentd

--- models/likedEvents.model.js
+++ models/likedEvents.model.js
@@ -4,16 +4,6 @@ const LikedEventsSchema = new mongoose.Schema({
   userId: String,
   eventId: String,
 });
-// const LikedEventsSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
-//   likedEvents: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: 'Events',
-//     },
-//   ],
-// });
-
 const LikedEvents = mongoose.model('LikedEvents', LikedEventsSchema);
 
 export default LikedEvents;
